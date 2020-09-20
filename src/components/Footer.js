baf5236129625dc6264808b63f20af1376b5c4ad
Artsiom Biba <Artsiom.Biba@mktg.com>
2020-09-20 10:12:23 -0400

    footer & landing updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
-import {
-  StaticQuery,
-  graphql
-} from 'gatsby';
-import {
-  Text,
-  Box,
-  Link,
-  Flex
-} from 'rebass/styled-components';
+import { StaticQuery, graphql } from 'gatsby';
+import { Text, Box, Link, Flex } from 'rebass/styled-components';
 import Fade from 'react-reveal/Fade';
 import SocialLink from './SocialLink';
 
-const FooterContainer = styled.div `
+const FooterContainer = styled.div`
   max-width: 1366px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   margin: auto;
-
   @media (max-width: 400px) {
     flex-direction: column-reverse;
-
     & > * {
       margin-bottom: 10px;
     }
   }
 `;
 
-const TextFooter = styled(Text)
-`
+const TextFooter = styled(Text)`
   color: ${(props) => props.theme.colors.background};
-
   & a {
     color: ${(props) => props.theme.colors.background};
     transition: color ease 0.5s;
-
     &:hover {
       color: ${(props) => props.theme.colors.primaryLight};
     }
   }
 `;
 
-const Footer = () => ( <
-  StaticQuery query = {
-    graphql `
+const Footer = () => (
+  <StaticQuery
+    query={graphql`
       query FooterQuery {
         contentfulAbout {
           name
@@ -59,71 +46,31 @@ const Footer = () => ( <
           }
         }
       }
-    `
-  }
-  render = {
-    (data) => {
-      const {
-        name,
-        socialLinks
-      } = data.contentfulAbout;
+    `}
+    render={(data) => {
+      const { name, socialLinks } = data.contentfulAbout;
 
-      return ( <
-        Box p = {
-          [2, 3]
-        }
-        backgroundColor = "primaryDark"
-        id = "footer" >
-        <
-        FooterContainer >
-        <
-        Fade left >
-        <
-        TextFooter fontSize = {
-          [2, 3]
-        } >
-        <
-        span > {
-          `${name}'s Portfolio`
-        } < /span> <
-        span role = "img"
-        aria label = "heart" > ❤️
-        <
-        /span> < /
-        TextFooter > <
-        /Fade> <
-        Flex >
-        <
-        Fade right > {
-          socialLinks.map(({
-            id,
-            ...rest
-          }) => ( <
-            Box mx = {
-              [2, 3]
-            }
-            fontSize = {
-              [4, 5]
-            }
-            key = {
-              id
-            } >
-            <
-            SocialLink {
-              ...rest
-            }
-            color = "background" / >
-            <
-            /Box>
-          ))
-        } <
-        /Fade> < /
-        Flex > <
-        /FooterContainer> < /
-        Box >
+      return (
+        <Box p={[2, 3]} backgroundColor="primaryDark" id="footer">
+          <FooterContainer>
+            <Fade left>
+              <TextFooter fontSize={[2, 3]}>
+                <span>{`${name}'s Portfolio 🖖`}</span>
+              </TextFooter>
+            </Fade>
+            <Flex>
+              <Fade right>
+                {socialLinks.map(({ id, ...rest }) => (
+                  <Box mx={[2, 3]} fontSize={[4, 5]} key={id}>
+                    <SocialLink {...rest} color="background" />
+                  </Box>
+                ))}
+              </Fade>
+            </Flex>
+          </FooterContainer>
+        </Box>
       );
-    }
-  }
+    }}
   />
 );
 
