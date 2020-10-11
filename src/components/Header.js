42b9eb0825258fc86d7b139e36ae840b2c82dc98
Artsiom Biba <biba.artsiom@gmail.com>
2020-10-11 12:12:50 -0400

    adding logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Flex, Image } from 'rebass/styled-components';
 import styled from 'styled-components';
 import { SectionLinks } from 'react-scroll-section';
 import RouteLink from './RouteLink';
-import Logo from './Logo/Portfolio.svg';
+import Logo from './Logo/AB.png';
 
 const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
 
@@ -22,64 +22,54 @@ const HeaderContainer = styled(Headroom)`
 `;
 
 const formatLinks = (allLinks) =>
-  Object.entries(allLinks).reduce(
-    (acc, [key, value]) => {
-      const isHome = key === 'home';
-      return isHome
-        ? {
-            ...acc,
-            home: value,
-          }
-        : {
-            ...acc,
-            links: [...acc.links, { name: capitalize(key), value }],
-          };
-    },
-    { links: [], home: null },
-  );
+	Object.entries(allLinks).reduce(
+		(acc, [ key, value ]) => {
+			const isHome = key === 'home';
+			return isHome
+				? {
+						...acc,
+						home : value
+					}
+				: {
+						...acc,
+						links : [ ...acc.links, { name: capitalize(key), value } ]
+					};
+		},
+		{ links: [], home: null }
+	);
 
 const Header = () => (
-  <HeaderContainer>
-    <Flex
-      flexWrap="wrap"
-      justifyContent="space-between"
-      alignItems="center"
-      p={3}
-    >
-      <SectionLinks>
-        {({ allLinks }) => {
-          const { home, links } = formatLinks(allLinks);
+	<HeaderContainer>
+		<Flex flexWrap="wrap" justifyContent="space-between" alignItems="center" p={3}>
+			<SectionLinks>
+				{({ allLinks }) => {
+					const { home, links } = formatLinks(allLinks);
 
-          const homeLink = home && (
-            <Image
-              src={Logo}
-              width="50px"
-              alt="Portfolio Logo"
-              onClick={home.onClick}
-              style={{
-                cursor: 'pointer',
-              }}
-            />
-          );
-          const navLinks = links.map(({ name, value }) => (
-            <RouteLink
-              key={name}
-              onClick={value.onClick}
-              selected={value.isSelected}
-              name={name}
-            />
-          ));
+					const homeLink = home && (
+						<Image
+							src={Logo}
+							width="50px"
+							alt="Portfolio Logo"
+							onClick={home.onClick}
+							style={{
+								cursor : 'pointer'
+							}}
+						/>
+					);
+					const navLinks = links.map(({ name, value }) => (
+						<RouteLink key={name} onClick={value.onClick} selected={value.isSelected} name={name} />
+					));
 
-          return (
-            <Fragment>
-              {homeLink}
-              <Flex mr={[0, 3, 5]}>{navLinks}</Flex>
-            </Fragment>
-          );
-        }}
-      </SectionLinks>
-    </Flex>
-  </HeaderContainer>
+					return (
+						<Fragment>
+							{homeLink}
+							<Flex mr={[ 0, 3, 5 ]}>{navLinks}</Flex>
+						</Fragment>
+					);
+				}}
+			</SectionLinks>
+		</Flex>
+	</HeaderContainer>
 );
 
 export default Header;
