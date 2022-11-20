feef733d4963ef41eb11eb225c1f84f5eda93341
Artsy Biba <biba.artsiom@gmail.com>
2022-11-20 15:44:10 -0500

    update weird formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,13 @@ import About from '../sections/About';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const IndexPage = () => ( <
-  Layout >
-  <
-  Header / >
-  <
-  Landing / >
-  <
-  About / >
-  <
-  Footer / >
-  <
-  /Layout>
+const IndexPage = () => ( 
+  <Layout>
+    <Header/>
+    <Landing/>
+    <About/>
+    <Footer/>
+  </Layout>
 );
 
 export default IndexPage;
