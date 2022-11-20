2be43003b86d92404240cfcdbebf5739e7573c11
Artsy Biba <biba.artsiom@gmail.com>
2022-11-20 15:40:29 -0500

    remove some sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
-import Projects from '../sections/Projects';
-import Writing from '../sections/Writing';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -13,8 +11,6 @@ const IndexPage = () => ( <
   Header / >
   <
   Landing / >
-  <
-  Projects / >
   <
   About / >
   <
